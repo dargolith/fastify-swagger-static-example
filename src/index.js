3fb05cef374c2b84485d316ff12b939f4332dfa7
Jesper Ahlberg <jesper@digitalfortress.se>
2024-01-25 13:42:20 +0100

    Add example code

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const fastify = require('fastify');
+const path = require('path');
+
+const start = async () => {
+  const app = fastify({});
+  // app.register(require('@fastify/static'), {
+  //   root: path.join(__dirname, '..', 'openapi'),
+  //   prefix: '/documentation',
+  // });
+  await app.register(require('@fastify/swagger'), {
+    mode: 'static',
+    specification: {
+      path: './openapi/index.yaml',
+    },
+  });
+  await app.register(require('@fastify/swagger-ui'), {
+    mode: 'static',
+  });
+  app.listen({ host: '0.0.0.0', port: 80 }, () => {
+    console.info('Server started.');
+  });
+  app.register((fastify) =>
+    fastify.post(
+      '/test',
+      (req) => `OK - arg1:${req.body.arg1}, arg2:${req.body.arg1}`
+    )
+  );
+};
+
+if (require.main === module) start();
